ae1cec2e60b38aba3dabce86cc307dc6a7e241b4
katherinedougan <k.dougan@uq.edu.au>
2023-05-19 20:48:57 +1000

    remove automatic build

--- build_sandbox.sh
+++ build_sandbox.sh
@@ -4,7 +4,7 @@ sudo singularity build --sandbox sandbox docker://ubuntu:22.04
 sudo singularity shell --writable sandbox/
 
 # To build from .def file:
-sudo singularity build singularity_image.sif singularity.def
+# sudo singularity build singularity_image.sif singularity.def
 
 # To build from sandbox:
-sudo singularity build singularity_image.sif sandbox/
+# sudo singularity build singularity_image.sif sandbox/
